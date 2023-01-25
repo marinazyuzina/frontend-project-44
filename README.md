05cedde4b712d633d4cfb6d3edd62e440d069bcd
Marina Zyuzina <zmarina526@gmail.com>
2023-01-25 19:33:43 +0300

    update Makefile, README.md, package.json, src/cli.js, src/even.js. Add bin/brain-even.js

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [![Actions Status](https://github.com/marinazyuzina/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/marinazyuzina/frontend-project-44/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/5c29695078235a2dc453/maintainability)](https://codeclimate.com/github/marinazyuzina/frontend-project-44/maintainability)
+
+<a href="https://asciinema.org/a/554328" target="_blank"><img src="https://asciinema.org/a/554328.svg" /></a>
--- src/even.js
+++ src/even.js
@@ -1,31 +1,23 @@
 import readlineSync from 'readline-sync';
+import greeting from './cli.js';
 
 const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-const greeting = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${name}!`);
-};
-greeting();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const question = 'Question:';
+const userName = greeting();
 
 const startGame = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const question = 'Question:';
   for (let i = 0; i < 3; i += 1) {
     const randomNum = getRandomNum(1, 100);
+    const correctAnswer = randomNum % 2 === 0 ? 'yes' : 'no';
     console.log(`${question} ${randomNum}`);
-    const answer = readlineSync.question('Your Answer: ');
-    if ((randomNum % 2 === 0 && answer === 'yes') || (randomNum % 2 !== 0 && answer === 'no')) {
-      console.log('Correct!');
-    } else if (randomNum % 2 === 0 && answer === 'no') {
-      console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
+    const userAnswer = readlineSync.question('Your Answer: ');
+    if (userAnswer !== correctAnswer) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
       return;
-    } else if (randomNum % 2 !== 0 && answer === 'yes') {
-      console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
-      return;
-    }
+    } console.log('Correct!');
   }
-  console.log(`Congratulations, ${name}`);
+  console.log(`Congratulations, ${userName}!`);
 };
-startGame();
+
+export default startGame;
